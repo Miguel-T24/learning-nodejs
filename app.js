3b5c0c7042201600a74819d749cc8537c09c47e3
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-06 16:20:59 -0500

    add exports and imoprt several modules

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const saludo = require("./saludo.js")
 
 console.log(saludo);
-console.log(saludo.saludar('Michael'));    
+console.log(saludo.saludar('Michael'));
+console.log(saludo.saludarHolaMundo())
--- saludo.js
+++ saludo.js
@@ -2,4 +2,14 @@ function saludar(nombre){
     return `hola ${nombre}`;
 }
 
-module.exports.saludar = saludar;
+function saludarHolaMundo(){
+    return 'Hola Mundo!'
+}
+
+// module.exports.saludar = saludar;
+// module.exports.saludarHolaMundo = saludarHolaMundo;
+
+module.exports = {
+    saludar : saludar,
+    saludarHolaMundo : saludarHolaMundo
+};
