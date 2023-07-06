de7c0293f67b35f42c9f896e0c8962c023488beb
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-06 16:34:13 -0500

    add modulo console

---
+++ Mconsole.js
@@ -0,0 +1,4 @@
+console.log('Hola mundo');
+console.warn("Ocurrio un error");
+console.error("Ocurrio un error");
+console.error(new Error('Ocurrio un error!'))
--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const saludo = require("./saludo.js")
+// const saludo = require("./saludo.js")
 
-console.log(saludo);
-console.log(saludo.saludar('Michael'));
-console.log(saludo.saludarHolaMundo())
+const {saludarHolaMundo, saludar} = require('./saludo.js');
+
+// console.log(saludo);
+// console.log(saludo.saludar('Michael'));
+console.log(saludarHolaMundo());
+console.log(saludar("Michael"));
