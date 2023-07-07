8788cd270059a16b74b9130f13f652e2ca436a5a
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-07 17:41:48 -0500

    example of promises

--- promesas.js
+++ promesas.js
@@ -1,21 +1,66 @@
-const promesaCumplida = false
+// const promesaCumplida = false
 
-const miPromesa = new Promise((resolve, reject) =>{
-    setTimeout(()=>{
-        if(promesaCumplida){
-            resolve('Promesa Cumplida');
-        }else{
-            reject('Promesa Rechazada');
+// const miPromesa = new Promise((resolve, reject) =>{
+//     setTimeout(()=>{
+//         if(promesaCumplida){
+//             resolve('Promesa Cumplida');
+//         }else{
+//             reject('Promesa Rechazada');
+//         }
+//     },3000);
+// });
+
+
+// const manejarPromesaCumplida = (razonCumplida) =>{
+//     console.log(razonCumplida)
+// }
+// const manejarPromesaRechazada = (razonRechazo) =>{
+//     console.log(razonRechazo);
+// }
+
+// miPromesa.then(manejarPromesaCumplida, manejarPromesaRechazada);
+
+// Ejercicio
+
+const estatusPedido = () => {
+    const estatus = Math.random() < 0.8;
+    return estatus;
+};
+
+// for (let i = 0 ; i < 10 ; i++){
+//     console.log(estatusPedido());
+// }
+
+
+
+const miPedidoPizza = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+        if(estatusPedido()){
+            resolve('Pizza esta en camino');
         }
-    },3000);
-});
-
+        else{
+            reject('La pizza se perdio!!');
+        }
+    }, 3000);
+})
 
 const manejarPromesaCumplida = (razonCumplida) =>{
-    console.log(razonCumplida)
-}
-const manejarPromesaRechazada = (razonRechazo) =>{
-    console.log(razonRechazo);
-}
+    console.log(razonCumplida);
+};
 
-miPromesa.then(manejarPromesaCumplida, manejarPromesaRechazada)
+const manejarPromesaRechazada = (razonRechazo)=>{
+    console.log(razonRechazo);
+};
+
+
+miPedidoPizza.then(manejarPromesaCumplida,manejarPromesaRechazada);
+
+
+// Utilizando un soolo bloque de codigo
+miPedidoPizza
+.then((manejarPromesaCumplida)=>{
+    console.log(manejarPromesaCumplida);
+})
+.catch((manejarPromesaRechazada)=>{
+    console.log(manejarPromesaRechazada);
+})
