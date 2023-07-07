70ab921961a3a38a3c726ca2d6239a47c23d133d
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-07 09:38:49 -0500

    add append content in a file

--- Modules/Mfs/app.js
+++ Modules/Mfs/app.js
@@ -1,10 +1,31 @@
 const fs = require('fs')
 
-fs.readFile('./index.html','utf-8',(err,contenido) => {
+// Read File
+// fs.readFile('./index.html','utf-8',(err,contenido) => {
+//     if(err){
+//         // console.log(err);
+//         throw(err) // Detiene la ejecucion del programa
+//     }else{
+//         console.log(contenido);
+//     }
+// });
+
+
+// rename
+// fs.rename('index.html','main.html', (err)=>{
+//     if(err){
+//         throw err;
+//     }else{
+//         console.log('Nombre cambiado exitosamente');
+//     }
+// })
+
+// Add a content to final of the file
+fs.appendFile('index.html','<p>Hola</p>', (err) =>{
     if(err){
-        // console.log(err);
-        throw(err) // Detiene la ejecucion del programa
+        throw err;
     }else{
-        console.log(contenido);
+        console.log('Archivo actualizado!')
     }
-});
+
+})
