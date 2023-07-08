ccd3bc8bf42f942dac34b3adfa3aca506af9d4c9
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-08 09:50:28 -0500

    add my first server basic

---
+++ http/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const servidor = http.createServer((req,res) =>{
+    // Realizar el proceso y al final llamamos a end y luego le pasamos como parametro el resultado que queremos al cliente
+    console.log("Solicitud nueva");
+    res.end('Hola, Mundo');
+});
+
+const PUERTO = 3000;
+
+servidor.listen(PUERTO, ()=>{
+    console.log(`El servidor esta escuchando en el puerto: ${PUERTO}`);
+})
