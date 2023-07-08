72a341ab3bd69734d39a5a21d58dc34930c5736d
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-08 12:40:17 -0500

    add routing in nodejs

---
+++ routing/app.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const cursos = require("./cursos");
+
+const server = http.createServer((req,res)=>{
+    const {method} = req;
+    switch(method){
+        case 'GET':
+            return manejarSolicitudGet(req,res);
+        case 'POST':
+            return manejarSolicitudPost(req,res);
+        default:
+            console.log(`El metodo usado no puede ser manejado por el servidor: ${method}`);
+    }
+});
+
+
+function manejarSolicitudGet(req, res){
+    const path = req.url;
+
+    if(path==='/'){
+        res.statusCode = 200;
+        res.end("Bienvenidos a mi primer servidor y API  creado con Node.js");
+    }else if (path === '/cursos'){
+        res.statusCode = 200;
+        return res.end(JSON.stringify(cursos.infoCursos));
+    }else if (path === '/cursos/programacion'){
+        res.statusCode = 200;
+        return res.end(JSON.stringify(cursos.infoCursos.programacion));
+    }else if (path === "/cursos/matematicas"){
+        res.statusCode = 200;
+        return es.end(JSON.stringify(cursos.infoCursos.matematicas))
+    }
+    res.statusCode = 404;
+    res.end("No se ha encontrado el recurso solicitado");
+}
+
+
+function manejarSolicitudPost(req, res){
+    const path = req.url;
+    if(path === '/cursos/programacion'){
+        res.statusCode = 200;
+        res.end('El servidor recibio una solicitud POST para /cursos/programacion');
+    }
+}
+
+
+PORT = 3000;
+
+server.listen(PORT,()=>{
+    console.log(`Ejecucion en el puerto ${PORT}`);
+})
---
+++ routing/cursos.js
@@ -0,0 +1,44 @@
+let infoCursos = {
+    'programacion':[
+        {
+            id : 1,
+            titulo : 'Aprende Python',
+            lenguaje : 'Python',
+            vistas : 15000,
+            nivel : 'basico'
+        },
+        {
+            id : 2,
+            titulo: 'Python Intermedio',
+            lenguaje: 'Python',
+            vistas: 13553,
+            nivel: 'intermedio',
+        },
+        {
+            id : 3,
+            titulo : 'Aprende Javascript',
+            lenguaje: 'Javascript',
+            vistas : 102223,
+            nivel: 'basico'
+        }
+    ],
+    
+    'matematicas':[
+        {
+            id : 1,
+            titulo: 'Aprende Calculo',
+            tema: 'Calculo',
+            vistas: 12427,
+            nivel: 'basico'
+        },
+        {
+            id : 2,
+            titulo : 'Aprende Algebra',
+            tema : 'Algebra',
+            vistas : 15722,
+            nivel: 'intermedio'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
