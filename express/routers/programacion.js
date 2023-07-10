d7d81f1f70faf4b5cfc219b9b5b528b831cfcd51
Miguel Tamayo <miguelt2296@gmail.com>
2023-07-10 15:49:59 -0500

    add get post put patch delete

--- express/routers/programacion.js
+++ express/routers/programacion.js
@@ -4,6 +4,8 @@ const {programacion} = require('../datos/cursos').infoCursos;
 
 // router programacion
 const routerProgramacion  = express.Router();
+// middleware
+routerProgramacion.use(express.json());
 
 routerProgramacion.get('/',(req,res)=>{
     res.send(JSON.stringify(programacion));
@@ -41,6 +43,52 @@ routerProgramacion.get('/:lenguaje/:nivel',(req,res)=>{
         return res.status(404).send(`No se encontro curso de ${lenguaje} del nivel ${nivel}`);
     }
     res.send(JSON.stringify(resultado));
+});
+
+
+routerProgramacion.post('/',(req,res)=>{
+    let cursoNuevo = req.body;
+    programacion.push(cursoNuevo);
+    res.send(JSON.stringify(programacion));
+});
+
+
+routerProgramacion.put('/:id',(req,res)=>{
+    const cursoActualizado = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if(indice >=0){
+        programacion[indice] = cursoActualizado;
+    }
+    res.send(JSON.stringify(programacion));
+});
+
+// Solo los valores que queremos modificar
+routerProgramacion.patch('/:id',(req,res)=>{
+    const infoActualizada = req.body;
+    const id = req.params.id;
+
+    const indice = programacion.findIndex(curso => curso.id == id);
+
+    if(indice>=0){
+        const cursoModificar = programacion[indice];
+        Object.assign(cursoModificar,infoActualizada);
+    }
+    res.send(JSON.stringify(programacion));
+})
+
+routerProgramacion.delete('/:id',(req,res)=>{
+    const id = req.params.id;
+
+    const idx = programacion.findIndex(curso => curso.id == id);
+
+    if(idx >= 0){
+        programacion.splice(idx,1);
+    }
+
+    res.send(JSON.stringify(programacion));
 });
 
 module.exports = routerProgramacion;
